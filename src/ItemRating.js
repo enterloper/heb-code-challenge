d5d99a3aff7cde8781d6f175814668df523f5919
Rich Boothe <richardjboothe@gmail.com>
2020-11-19 15:53:34 -0600

    add stars for ratings

--- src/ItemRating.js
+++ src/ItemRating.js
@@ -3,12 +3,12 @@ import {number} from 'prop-types';
 
 const ItemRating = ({ rating }) => {
   return (
-    <div className="item_detail">
-      <p>{rating ? `Rating: ${rating}` : "Rating unavailable"}</p>
-    </div>
+    <div className="stars" style={{['--rating']: rating}} aria-label={`Rating of this product is ${rating} out of 5.`} />
   );
 };
-
+{/* <div className="item_detail">
+<p>{rating ? `Rating: ${rating}` : "Rating unavailable"}</p>
+</div> */}
 ItemRating.propTypes = {
   rating: number
 }
